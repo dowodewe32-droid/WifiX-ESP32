b29cb065d9481ace924d50e536fc2619c7db3f6d
WifiX Builder <build@wifix.local>
2026-04-23 05:22:15 +0000

    Move to src folder

--- config.h
+++
@@ -1,8 +0,0 @@
-#define AP_SSID "GMpro"
-#define AP_PASSWD "Sangkur87"
-#define DEAUTHER_VERSION "1.5.0"
-#define STROBE 2
-#define NYALA HIGH
-#define MATI LOW
-#define EEPROM_SIZE 4095
-#define WEB_IP_ADDR 192,168,4,1
