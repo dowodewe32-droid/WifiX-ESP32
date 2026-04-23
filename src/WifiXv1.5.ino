2344179375b56d8207a2baccbdd3c5e38ca1e46c
WifiX Builder <build@wifix.local>
2026-04-23 05:49:30 +0000

    Fix linker

--- src/WifiXv1.5.ino
+++ src/WifiXv1.5.ino
@@ -21,7 +21,6 @@ int foundNetworks = 0;
 String scanResults = "";
 
 esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
-extern "C" int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3) { return 0; }
 
 uint8_t deauthPacket[] = {
   0xC0, 0x00, 0x3A, 0x01,
